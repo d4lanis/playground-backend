e8895fec88c3a653753064d441177b7fb73967fb
Daniel Alanis <daniel.alanis@clickittech.com.mx>
2023-01-12 14:25:33 -0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import { responseEnhancer } from 'express-response-formatter';
+import logger from 'morgan';
+import * as dotenv from 'dotenv';
+import { port } from './const/const';
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(responseEnhancer());
+app.use(cors());
+app.use(logger("dev"));
+dotenv.config();
+
+app.get('/', (req, res) => {
+  res.formatter.ok("Welcome to Nearby-Eats - Backend")
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
