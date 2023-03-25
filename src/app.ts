6f1b7725090ec2c37916eb4e038dfb7f2604cd94
Daniel Alanis <daniel.alanis.hdz@gmail.com>
2023-03-25 06:21:22 +0000

    use cross-fetch to search words on the wikipedia API

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-import express from 'express';
+import express, { Request, Response} from 'express';
 import cors from 'cors';
 import { responseEnhancer } from 'express-response-formatter';
 import logger from 'morgan';
 import * as dotenv from 'dotenv';
+import fetch from "cross-fetch";
 import { port } from './const/const';
+import { url } from './const/const';
+
 const app = express();
 
 app.use(express.json());
@@ -13,10 +16,24 @@ app.use(cors());
 app.use(logger("dev"));
 dotenv.config();
 
-app.get('/', (req, res) => {
-  res.formatter.ok("Welcome to Nearby-Eats - Backend")
+app.get('/', (req: Request, res: Response) => {
+  return res.formatter.ok("Welcome to Alanis - Backend")
+});
+
+app.get('/api/search/:term', (req: Request, res: Response) => {
+  const { term } = req.params;
+
+  fetch(`${url}${term}`)
+    .then( response => response.json())
+    .then( data => {
+      return res.formatter.ok(data);
+    })
+    .catch(error => {
+      return res.formatter.badRequest(error);
+      
+    });
 });
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+  return console.log(`Express is listening at PORT:${port}`);
 });
--- src/const/const.ts
+++ src/const/const.ts
@@ -1,1 +1,2 @@
 export const port = process.env.PORT || 3000;
+export const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=`
